35680a015e9c4fd736c43fded08edfef2a19b7b4
MichaelMireku <mirekumichaelk@gmail.com>
2025-02-14 19:14:01 +0000

    ty

--- script.js
+++ script.js
@@ -16,53 +16,53 @@ function goHome() {
 }
 
 // WMP
-const video = document.getElementById('video');
-const playPause = document.getElementById('playPause');
-const stop = document.getElementById('stop');
-const seek = document.getElementById('seek');
-const volume = document.getElementById('volume');
-const fullscreen = document.getElementById('fullscreen');
-const trackTitle = document.getElementById('trackTitle');
+const video = document.getElementById("video");
+const playPause = document.getElementById("playPause");
+const stop = document.getElementById("stop");
+const seek = document.getElementById("seek");
+const volume = document.getElementById("volume");
+const fullscreen = document.getElementById("fullscreen");
+const trackTitle = document.getElementById("trackTitle");
 
 // Play / Pause functionality
-playPause.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-        playPause.innerHTML = '<img src="pause.png" alt="Pause">';
-    } else {
-        video.pause();
-        playPause.innerHTML = '<img src="play.png" alt="Play">';
-    }
+playPause.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    playPause.innerHTML = '<img src="pause.png" alt="Pause">';
+  } else {
+    video.pause();
+    playPause.innerHTML = '<img src="play.png" alt="Play">';
+  }
 });
 
 // Stop functionality
-stop.addEventListener('click', () => {
-    video.pause();
-    video.currentTime = 0;
-    playPause.innerHTML = '<img src="play.png" alt="Play">';
+stop.addEventListener("click", () => {
+  video.pause();
+  video.currentTime = 0;
+  playPause.innerHTML = '<img src="play.png" alt="Play">';
 });
 
 // Seek bar update
-video.addEventListener('timeupdate', () => {
-    seek.value = (video.currentTime / video.duration) * 100;
+video.addEventListener("timeupdate", () => {
+  seek.value = (video.currentTime / video.duration) * 100;
 });
 
-seek.addEventListener('input', () => {
-    video.currentTime = (seek.value / 100) * video.duration;
+seek.addEventListener("input", () => {
+  video.currentTime = (seek.value / 100) * video.duration;
 });
 
 // Volume control
-volume.addEventListener('input', () => {
-    video.volume = volume.value;
+volume.addEventListener("input", () => {
+  video.volume = volume.value;
 });
 
 // Fullscreen functionality
-fullscreen.addEventListener('click', () => {
-    if (video.requestFullscreen) {
-        video.requestFullscreen();
-    } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) {
-        video.msRequestFullscreen();
-    }
+fullscreen.addEventListener("click", () => {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
 });
